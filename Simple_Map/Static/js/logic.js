276b0b8daa46506da32b622b2f269d4c2b287325
Katelyn Galer <kgaler@luminexcorp.com>
2021-07-09 21:14:21 -0500

    simple map add on branch

---
+++ Simple_Map/Static/js/logic.js
@@ -0,0 +1,27 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//title layer
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11',
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+//Other styles:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
+
+// Then we add our streets to the title layer
+streets.addTo(map);
