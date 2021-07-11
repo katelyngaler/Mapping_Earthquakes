96e39478bc9e683b7f0b966b5c5cb5ff634d143f
Katelyn Galer <kgaler@luminexcorp.com>
2021-07-10 22:04:43 -0500

    updated logic for circle marker

--- Mapping_Single_Points/Static/js/logic.js
+++ Mapping_Single_Points/Static/js/logic.js
@@ -2,11 +2,17 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.circleMarker([34.0522, -118.2437],{
+    color: "black",
+    fillColor: "#ffffa1",
+    radius: 300
+}).addTo(map);
 
 //title layer
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     // id: 'mapbox/streets-v11',
