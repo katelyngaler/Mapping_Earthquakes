0118b769a7fca8fc10b108fb8e97406cedf46eb7
Katelyn Galer <kgaler@luminexcorp.com>
2021-07-12 21:37:13 -0500

    adding toronto routes

---
+++ Mapping_GeoJSON_Linestrings/Static/js/logic.js
@@ -0,0 +1,58 @@
+
+
+//title layer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11',
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid',{
+  center: [30, 30], 
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Access airport Geo JSON URL
+let airportData = "https://raw.githubusercontent.com/katelyngaler/Mapping_Earthquakes/main/majorAirports.json"
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h2>Airport code: " + feature.properties.faa  + "</h2>" + "<hr><h3>Airport name: " + feature.properties.name + "</h3>")
+  }
+
+}).addTo(map);
+});
+
+
+
+//Other styles:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
+
