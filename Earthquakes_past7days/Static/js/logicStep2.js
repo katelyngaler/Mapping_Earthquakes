95d510f7a527b25b12453995e438642eef19b194
Katelyn Galer <kgaler@luminexcorp.com>
2021-07-13 21:49:49 -0500

    13.6.3

--- Earthquakes_past7days/Static/js/logicStep2.js
+++ Earthquakes_past7days/Static/js/logicStep2.js
@@ -32,13 +32,47 @@ let map = L.map('mapid',{
 L.control.layers(baseMaps).addTo(map);
 
 
+// Create a style for the lines.
+let myStyle = {
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
+}
 
+// function for style
+function styleInfo(feature){
+  return{
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+
+// radius based on magnitude
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
 
 // Grabbing our GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 //  console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+  pointToLayer: function(feature, latlng){
+    console.log(data);
+    return L.circleMarker(latlng)
+  },
+  style: styleInfo
+
+  //  .layer.bindPopup("<h2>Airport code: " + feature.properties.faa  + "</h2>" + "<hr><h3>Airport name: " + feature.properties.name + "</h3>")
+  }).addTo(map);
 
 });
 
--- Earthquakes_past7days/Static/js/logicStep3.js
+++ Earthquakes_past7days/Static/js/logicStep3.js
@@ -32,13 +32,61 @@ let map = L.map('mapid',{
 L.control.layers(baseMaps).addTo(map);
 
 
+// function for style
+function styleInfo(feature){
+  return{
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
 
+// radius based on magnitude
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+// function to determine color based on magnitude
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
 
 // Grabbing our GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 //  console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+  pointToLayer: function(feature, latlng){
+    console.log(data);
+    return L.circleMarker(latlng)
+  },
+  style: styleInfo,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup(`Magnitude: ${feature.properties.mag} <br> Location: ${feature.properties.place}`);
+  }
+  }).addTo(map);
 
 });
 
